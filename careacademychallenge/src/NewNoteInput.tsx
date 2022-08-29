2236fb5f13c6508752710c347fa95d954a392979
Joosawe <Josawe1904@gmail.com>
2022-08-29 16:55:56 -0400

    Static edit / delete buttons

--- careacademychallenge/src/NewNoteInput.tsx
+++ careacademychallenge/src/NewNoteInput.tsx
@@ -1,4 +1,5 @@
 import React, { ChangeEvent } from "react";
+import "./NewNoteInput.css";
 
 interface NewNoteInputProps {
   addNote(note: string): void;
@@ -16,16 +17,24 @@ export const NewNoteInput: React.FC<NewNoteInputProps> = ({ addNote }) => {
     setNote("");
   };
 
+  
+
   return (
+    <>
+    <h1 className = "h1"> Note Editor</h1>
     <div>
-      <input
+      <input className="input"
         onChange={updateNote}
         value={note}
         type="text"
         name="note"
-        placeholder="Note"
+        placeholder="Enter New Note"
       />
-      <button onClick={onAddNoteClick}>Add note</button>
+      <button className = "btn" onClick={onAddNoteClick}>Add note</button>
+      <button className = "btn">Edit note</button>
+      <button className = "btn" >Delete note</button>
+      
     </div>
+    </>
   );
 };
